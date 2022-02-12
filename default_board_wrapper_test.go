ef01e0bcfe5c09e1c9a7702fc4d094ce758cb889
thewizardplusplus <thewizardplusplus@yandex.ru>
2022-02-12 23:00:00 +0300

    Add the tests for the `chessmodels.defaultBoardWrapper.CheckMove()` method

--- default_board_wrapper_test.go
+++ default_board_wrapper_test.go
@@ -52,3 +52,180 @@ func TestDefaultBoardWrapperPieces(test *testing.T) {
 		test.Fail()
 	}
 }
+
+func TestDefaultBoardWrapperCheckMove(test *testing.T) {
+	type fields struct {
+		size  Size
+		piece func(position Position) (piece Piece, ok bool)
+	}
+	type args struct {
+		move Move
+	}
+	type data struct {
+		fields fields
+		args   args
+		want   error
+	}
+
+	for _, data := range []data{
+		{
+			fields: fields{
+				size: Size{2, 2},
+				piece: func(position Position) (piece Piece, ok bool) {
+					return nil, false
+				},
+			},
+			args: args{
+				move: Move{
+					Start:  Position{0, 0},
+					Finish: Position{0, 0},
+				},
+			},
+			want: ErrNoMove,
+		},
+		{
+			fields: fields{
+				size: Size{2, 2},
+				piece: func(position Position) (piece Piece, ok bool) {
+					return nil, false
+				},
+			},
+			args: args{
+				move: Move{
+					Start:  Position{0, 0},
+					Finish: Position{-1, -1},
+				},
+			},
+			want: ErrOutOfSize,
+		},
+		{
+			fields: fields{
+				size: Size{2, 2},
+				piece: func(position Position) (piece Piece, ok bool) {
+					return nil, false
+				},
+			},
+			args: args{
+				move: Move{
+					Start:  Position{0, 0},
+					Finish: Position{1, 1},
+				},
+			},
+			want: ErrNoPiece,
+		},
+		{
+			fields: fields{
+				size: Size{2, 2},
+				piece: func(position Position) (piece Piece, ok bool) {
+					if position != (Position{0, 0}) && position != (Position{1, 1}) {
+						return nil, false
+					}
+
+					piece = MockPiece{color: Black, position: position}
+					return piece, true
+				},
+			},
+			args: args{
+				move: Move{
+					Start:  Position{0, 0},
+					Finish: Position{1, 1},
+				},
+			},
+			want: ErrFriendlyTarget,
+		},
+		{
+			fields: fields{
+				size: Size{2, 2},
+				piece: func(position Position) (piece Piece, ok bool) {
+					if position != (Position{0, 0}) {
+						return nil, false
+					}
+
+					piece = MockPiece{
+						position: position,
+						checkMove: func(move Move, storage PieceStorage) bool {
+							return false
+						},
+					}
+					return piece, true
+				},
+			},
+			args: args{
+				move: Move{
+					Start:  Position{0, 0},
+					Finish: Position{1, 1},
+				},
+			},
+			want: ErrIllegalMove,
+		},
+		{
+			fields: fields{
+				size: Size{2, 2},
+				piece: func(position Position) (piece Piece, ok bool) {
+					switch position {
+					case Position{0, 0}:
+						piece = MockPiece{
+							color:    Black,
+							position: Position{0, 0},
+							checkMove: func(move Move, storage PieceStorage) bool {
+								return true
+							},
+						}
+					case Position{1, 1}:
+						piece = MockPiece{
+							kind:     King,
+							color:    White,
+							position: Position{1, 1},
+						}
+					}
+
+					ok = piece != nil
+					return piece, ok
+				},
+			},
+			args: args{
+				move: Move{
+					Start:  Position{0, 0},
+					Finish: Position{1, 1},
+				},
+			},
+			want: ErrKingCapture,
+		},
+		{
+			fields: fields{
+				size: Size{2, 2},
+				piece: func(position Position) (piece Piece, ok bool) {
+					if position != (Position{0, 0}) {
+						return nil, false
+					}
+
+					piece = MockPiece{
+						position: position,
+						checkMove: func(move Move, storage PieceStorage) bool {
+							return true
+						},
+					}
+					return piece, true
+				},
+			},
+			args: args{
+				move: Move{
+					Start:  Position{0, 0},
+					Finish: Position{1, 1},
+				},
+			},
+			want: nil,
+		},
+	} {
+		baseStorage := MockBasePieceStorage{
+			size:  data.fields.size,
+			piece: data.fields.piece,
+		}
+		board := defaultBoardWrapper{baseStorage}
+		got := board.CheckMove(data.args.move)
+
+		if got != data.want {
+			test.Fail()
+		}
+	}
+}
