00373be67b463de08f10c001b5385a5a3b8180e6
thewizardplusplus <thewizardplusplus@yandex.ru>
2022-03-10 23:00:00 +0300

    Improve the documentation of the aliases

--- aliases.go
+++ aliases.go
@@ -6,48 +6,73 @@ import (
 )
 
 // Color ...
+//
+// Deprecated: use [common.Color] instead.
 type Color = common.Color
 
 // ...
 const (
-	Black = common.Black
-	White = common.White
+	// Deprecated: use common.Black instead.
+	Black Color = common.Black
+	// Deprecated: use common.White instead.
+	White Color = common.White
 )
 
 // Kind ...
+//
+// Deprecated: use [common.Kind] instead.
 type Kind = common.Kind
 
 // ...
 const (
-	King   = common.King
-	Queen  = common.Queen
-	Rook   = common.Rook
-	Bishop = common.Bishop
-	Knight = common.Knight
-	Pawn   = common.Pawn
+	// Deprecated: use common.King instead.
+	King Kind = common.King
+	// Deprecated: use common.Queen instead.
+	Queen Kind = common.Queen
+	// Deprecated: use common.Rook instead.
+	Rook Kind = common.Rook
+	// Deprecated: use common.Bishop instead.
+	Bishop Kind = common.Bishop
+	// Deprecated: use common.Knight instead.
+	Knight Kind = common.Knight
+	// Deprecated: use common.Pawn instead.
+	Pawn Kind = common.Pawn
 )
 
 // Position ...
+//
+// Deprecated: use [common.Position] instead.
 type Position = common.Position
 
 // Move ...
+//
+// Deprecated: use [common.Move] instead.
 type Move = common.Move
 
 // Size ...
+//
+// Deprecated: use [common.Size] instead.
 type Size = common.Size
 
 // Piece ...
+//
+// Deprecated: use [common.Piece] instead.
 type Piece = common.Piece
 
 // PieceStorage ...
+//
+// Deprecated: use [common.PieceStorage] instead.
 type PieceStorage = common.PieceStorage
 
 // ...
 var (
+	// Deprecated: use common.ErrKingCapture instead.
 	ErrKingCapture = common.ErrKingCapture
 )
 
 // NewBoard ...
+//
+// Deprecated: use [boards.NewSliceBoard] instead.
 func NewBoard(size common.Size, pieces []common.Piece) common.PieceStorage {
 	return boards.NewSliceBoard(size, pieces)
 }
