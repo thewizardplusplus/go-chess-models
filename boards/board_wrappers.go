a69647e19545fe1d6286467e8e0a57902c99c8fc
thewizardplusplus <thewizardplusplus@yandex.ru>
2022-04-07 23:00:00 +0300

    Remove the `boards.DefaultBoardWrapper` structure

--- boards/board_wrappers.go
+++ boards/board_wrappers.go
@@ -63,28 +63,3 @@ func WrapBasePieceStorage(
 		return pieceStorageWrapper{baseStorage}
 	}
 }
-
-// DefaultBoardWrapper ...
-type DefaultBoardWrapper struct {
-	common.BasePieceStorage
-}
-
-// Pieces ...
-func (board DefaultBoardWrapper) Pieces() []common.Piece {
-	if pieceGroupGetter, ok := board.BasePieceStorage.(common.PieceGroupGetter); ok {
-		return pieceGroupGetter.Pieces()
-	}
-
-	return common.Pieces(board)
-}
-
-// CheckMove ...
-//
-// It doesn't check for a check before or after the move.
-func (board DefaultBoardWrapper) CheckMove(move common.Move) error {
-	if moveChecker, ok := board.BasePieceStorage.(common.MoveChecker); ok {
-		return moveChecker.CheckMove(move)
-	}
-
-	return common.CheckMove(board, move)
-}
